a492f88e547d7f566a790a79909acde518ec8a96
jybill01 <jdragon0151@gmail.com>
2021-10-06 21:22:27 +0900

    Delete and reconnect

--- models/index.js
+++ models/index.js
@@ -1,6 +1,4 @@
 const Sequelize = require('sequelize');
-const Notice = require('./notice');
-const User = require('./user');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -9,14 +7,5 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
-
-db.Notice = Notice;
-db.User = User;
-
-Notice.init(sequelize);
-User.init(sequelize);
-
-Notice.associate(db);
-User.associate(db);
 
 module.exports = db;
