f47d102261d34598063cec980eddb42bcb587a3e
jybill01 <jdragon0151@gmail.com>
2021-11-01 17:13:42 +0900

    data push & view

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.route('/')
+  .get(async (req, res, next) => {
+    try {
+      const users = await User.findAll();
+      res.json(users);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      const user = await User.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      console.log(user);
+      res.status(201).json(user);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  });
+
+module.exports = router;
